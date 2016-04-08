c9b42d8d3a53b44c6e941c344b0e5fa7264ba00f
yugo4k <yugo4k@gmail.com>
2016-04-07 23:12:41 -0300

    report update

---
+++ exercicio2/README.md
@@ -0,0 +1,19 @@
+### Exercício 2: Configuração de Cache
+#### 033324 - Yugo Kuno
+
+---
+
+Para esta avaliar a melhor configuração de cache para um programa, foi simulada com o software [dinero](http://www.cs.wisc.edu/~markhill/DineroIV/) as operações de cache do programa gcc a partir dos traces 176.gcc.f2b e 176.gcc.m2b disponíveis no seguinte [endereço da rede do IC](~lucas/mc723/traces).
+
+### Avaliação de _replacement policy_
+
+|trname|parameter|miss rate l1i|miss rate l1d|miss rate l2u|
+|---|---|---|---|---|
+|gcc_f2b|repl=l|0.002400|0.007800|0.504500|
+|gcc_f2b|repl=f|0.002600|0.008400|0.524900|
+|gcc_f2b|repl=r|0.002500|0.008600|0.497700|
+|gcc_m2b|repl=l|0.000100|0.037400|0.995700|
+|gcc_m2b|repl=f|0.000100|0.037400|0.995300|
+|gcc_m2b|repl=r|0.000100|0.037600|0.867900|
+
+(l=LRU, f=FIFO, r=random)
--- exercicio2/script.py
+++ exercicio2/script.py
@@ -5,73 +5,110 @@ import subprocess as sp
 
 def main():
     traces = [('/home/staff/lucas/mc723/traces/176.gcc.f2b/', 'gcc_f2b'), ('/home/staff/lucas/mc723/traces/176.gcc.m2b', 'gcc_m2b')]
-    for tracepath, trname in traces:
-        os.chdir(tracepath)
-        command  = '../../dinero4sbc/dineroIV'
-        args = [command]
-        args += ['-informat', 's']
-        args += ['-trname', trname]
-        args += ['-maxtrace', '20']
+    for i, (tracepath, trname) in enumerate(traces):
+        # for j, repl in enumerate(['l', 'f', 'r']):
+        #     varname = 'repl'
+        #     var = 'repl=%s' % repl
+        # for j, fetch in enumerate(['d', 'a', 'm', 't', 'l']):
+        #     varname = 'fetch'
+        #     var = 'fetch=%s' % fetch
+        for j, walloc in enumerate(['a', 'n', 'f']):
+            varname = 'walloc'
+            var = 'walloc=%s' % walloc
+        # for j, wback in enumerate(['a', 'n', 'f']):
+            os.chdir(tracepath)
+            command  = '../../dinero4sbc/dineroIV'
+            args = [command]
+            args += ['-informat', 's']
+            args += ['-trname', trname]
+            args += ['-maxtrace', '20']
 
-        repl = 'l'
-        fetch = 'd'
-        walloc = 'a'
-        wback = 'a'
+            repl = 'l'
+            fetch = 'l'
+            # walloc = 'a'
+            wback = 'a'
 
-        l1isize = '64K'
-        l1ibsize = '64'
-        l1iassoc = '2'
-        l1irepl = repl
-        l1ifetch = fetch
-        l1iwalloc = walloc
-        l1iwback = wback
+            l1isize = '64K'
+            l1ibsize = '64'
+            l1iassoc = '2'
+            l1irepl = repl
+            l1ifetch = fetch
+            l1iwalloc = walloc
+            l1iwback = wback
 
-        l1dsize = '64K'
-        l1dbsize = '64'
-        l1dassoc = '2'
-        l1drepl = repl
-        l1dfetch = fetch
-        l1dwalloc = walloc
-        l1dwback = wback
+            l1dsize = '64K'
+            l1dbsize = '64'
+            l1dassoc = '2'
+            l1drepl = repl
+            l1dfetch = fetch
+            l1dwalloc = walloc
+            l1dwback = wback
 
-        l2usize = '64K'
-        l2ubsize = '64'
-        l2uassoc = '16'
-        l2urepl = repl
-        l2ufetch = fetch
-        l2uwalloc = walloc
-        l2uwback = wback
+            l2usize = '64K'
+            l2ubsize = '64'
+            l2uassoc = '16'
+            l2urepl = repl
+            l2ufetch = fetch
+            l2uwalloc = walloc
+            l2uwback = wback
 
-        args += ['-l1-isize', l1isize]
-        args += ['-l1-ibsize', l1ibsize]
-        # args += ['-l1-iassoc', l1iassoc]
-        # args += ['-l1-irepl', l1irepl]
-        # args += ['-l1-ifetch', l1ifetch]
-        # args += ['-l1-iwalloc', l1iwalloc]
-        # args += ['-l1-iwback', l1iwback]
+            args += ['-l1-isize', l1isize]
+            args += ['-l1-ibsize', l1ibsize]
+            args += ['-l1-iassoc', l1iassoc]
+            args += ['-l1-irepl', l1irepl]
+            args += ['-l1-ifetch', l1ifetch]
+            # args += ['-l1-iwalloc', l1iwalloc]
+            # args += ['-l1-iwback', l1iwback]
 
-        args += ['-l1-dsize', l1dsize]
-        args += ['-l1-dbsize', l1dbsize]
-        # args += ['-l1-dassoc', l1dassoc]
-        # args += ['-l1-drepl', l1drepl]
-        # args += ['-l1-dfetch', l1dfetch]
-        # args += ['-l1-dwalloc', l1dwalloc]
-        # args += ['-l1-dwback', l1dwback]
+            args += ['-l1-dsize', l1dsize]
+            args += ['-l1-dbsize', l1dbsize]
+            args += ['-l1-dassoc', l1dassoc]
+            args += ['-l1-drepl', l1drepl]
+            args += ['-l1-dfetch', l1dfetch]
+            # args += ['-l1-dwalloc', l1dwalloc]
+            # args += ['-l1-dwback', l1dwback]
 
-        args += ['-l2-usize', l2usize]
-        args += ['-l2-ubsize', l2ubsize]
-        # args += ['-l2-uassoc', l2uassoc]
-        # args += ['-l2-urepl', l2urepl]
-        # args += ['-l2-ufetch', l2ufetch]
-        # args += ['-l2-uwalloc', l2uwalloc]
-        # args += ['-l2-uwback', l2uwback]
+            args += ['-l2-usize', l2usize]
+            args += ['-l2-ubsize', l2ubsize]
+            args += ['-l2-uassoc', l2uassoc]
+            args += ['-l2-urepl', l2urepl]
+            args += ['-l2-ufetch', l2ufetch]
+            # args += ['-l2-uwalloc', l2uwalloc]
+            # args += ['-l2-uwback', l2uwback]
 
-        print(' '.join(args))
+            if i == 0 and j == 0:
+                output_line = ' '.join(args)
+                print(output_line)
+                output_text = output_line + '\n'
+                output_line = '|trname|parameter|miss rate l1i|miss rate l1d|miss rate l2u|'
+                print(output_line)
+                output_text += output_line + '\n'
+                output_line = '|---|---|---|---|---|'
+                print(output_line)
+                output_text += output_line + '\n'
 
-        proc = sp.Popen(args, stdout=sp.PIPE, stderr=sp.PIPE)
-        out, err = proc.communicate()
-        print(out)
-        print(err)
+            proc = sp.Popen(args, stdout=sp.PIPE, stderr=sp.PIPE)
+            out, err = proc.communicate()
+            counter = 0
+            list_cache = ['l1-i', 'l1-d', 'l2-u']
+            for line in out.decode('utf-8').splitlines():
+                if 'rate' in line:
+                    if counter == 0:
+                        rate_l1i = float(line.split()[3])
+                    elif counter == 1:
+                        rate_l1d = float(line.split()[3])
+                    elif counter == 2:
+                        rate_l2u = float(line.split()[3])
+                    counter += 1
+            output_line = ('|%s|%s|%f|%f|%f|' % (trname, var, rate_l1i, rate_l1d, rate_l2u))
+            print(output_line)
+            output_text += output_line + '\n'
+            # print('err:', err.decode('utf-8'))
+    print(output_text)
+    fout = open('/home/ec2014/ra033324/Desktop/mc723a/git_2016s1.mc723a/exercicio2/' + varname + '.txt', 'w')
+    fout.write(output_text)
+    fout.close()
+
 
 if __name__ == '__main__':
     main()
