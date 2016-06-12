cc9dfb7966cf22e9d8187652e36ff7ac2ea5b8cf
yugo4k <yugo4k@gmail.com>
2016-06-12 14:03:20 -0300

    Updated the shell scripts and used hello.c to do the 'Basic peripheral' part of the assignment.

--- exercicio4/y4k/compile_hello_mips.sh
+++ exercicio4/y4k/compile_hello_mips.sh
@@ -4,5 +4,5 @@ export LD_LIBRARY_PATH=/home/staff/lucas/mc723/hw_tools/systemc-2.3.1/lib-linux6
 export PATH=$PATH:/home/staff/lucas/mc723/hw_tools/ArchC-2.2.0/installed/bin:/home/staff/lucas/mc723/hw_tools/mips-newlib-elf/bin/
 
 echo
-echo "mips-newlib-elf-gcc -specs=archc /home/ec2014/ra033324/y4k/classes/mc723a/exercicio4_local/y4k/hello.c -o /home/ec2014/ra033324/y4k/classes/mc723a/exercicio4_local/y4k/hello.mips"
-mips-newlib-elf-gcc -specs=archc /home/ec2014/ra033324/y4k/classes/mc723a/exercicio4_local/y4k/hello.c -o /home/ec2014/ra033324/y4k/classes/mc723a/exercicio4_local/y4k/hello.mips
+echo "mips-newlib-elf-gcc -specs=archc /home/ec2014/ra033324/y4k/classes/mc723a/git_2016s1.mc723a/exercicio4/y4k/hello.c -o /home/ec2014/ra033324/y4k/classes/mc723a/git_2016s1.mc723a/exercicio4/y4k/hello.mips"
+mips-newlib-elf-gcc -specs=archc /home/ec2014/ra033324/y4k/classes/mc723a/git_2016s1.mc723a/exercicio4/y4k/hello.c -o /home/ec2014/ra033324/y4k/classes/mc723a/git_2016s1.mc723a/exercicio4/y4k/hello.mips
--- exercicio4/y4k/compile_mips.sh
+++ exercicio4/y4k/compile_mips.sh
@@ -3,7 +3,7 @@
 export LD_LIBRARY_PATH=/home/staff/lucas/mc723/hw_tools/systemc-2.3.1/lib-linux64/:$LD_LIBRARY_PATH
 export PATH=$PATH:/home/staff/lucas/mc723/hw_tools/ArchC-2.2.0/installed/bin:/home/staff/lucas/mc723/hw_tools/mips-newlib-elf/bin/
 
-MIPSTLMPATH='/home/ec2014/ra033324/y4k/classes/mc723a/exercicio4_local/mips-tlm/'
+MIPSTLMPATH='/home/ec2014/ra033324/y4k/classes/mc723a/git_2016s1.mc723a/exercicio4/mips-tlm/'
 
 echo
 echo "cd ${MIPSTLMPATH}/"
